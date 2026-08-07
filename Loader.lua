429b722784f308ae8fe2f256462d971706e43e5e
Script-hub <scriptb.hub@gmail.com>
2026-08-07 12:31:45 +0700

    Update script URL and modify comment in Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,2 +1,2 @@
---มึงจะขโมยสคริปของกูหรอกูไม่ให้หรอก🖕🖕
-loadstring(game:HttpGet("https://prometheus.bonkhub.online/raw/e3f17956b2661309"))()
+--มึงจะขโมยสคริปของกูหรอกูไม่ให้หรอกมึงหาวิธีเอาเองนะไอ้สัส🖕🖕
+loadstring(game:HttpGet("https://prometheus.bonkhub.online/raw/725a33ff78c48683"))()
